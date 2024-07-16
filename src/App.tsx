c4bb6df983d1775b3b461cb1741af6c09bfd20e6
Shams <shamsmedhat1@gmail.com>
2024-07-16 10:08:16 +0300

    sec commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Applayout from "./pages/Applayout.tsx";
 import Home from "./pages/Home.tsx";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Applayout />,
-        children: [
-            {
-                index: true,
-                element: <Home />,
-            },
-            {
-                path: "transaction-analysis",
-                element: <TransactionsChart />,
-            },
-        ],
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Applayout />,
+            children: [
+                {
+                    index: true,
+                    element: <Home />,
+                },
+                {
+                    path: "transaction-analysis",
+                    element: <TransactionsChart />,
+                },
+            ],
+        },
+    ],
+    { basename: "/route-task/" }
+);
 
 function App() {
     return <RouterProvider router={router} />;
